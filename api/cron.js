169a2d50c74e5a0f3a869dfe8dab927dad1509ed
Ovishek Paul <ovishek.paul@gmail.com>
2023-04-20 22:59:46 +0600

    fix cron job

--- api/cron.js
+++ api/cron.js
@@ -1,39 +1,38 @@
 import { getAllRecords } from "../fetchData/airtable";
 import { csvKeys, fetchData } from "../fetchData/fetchData";
-import request from "request";
+import axios from "axios";
+import qs from "qs";
 
 const client_id = process.env.clientID; // Your client id
 const client_secret = process.env.clientSecret; // Your secret
 
 export default async function handler(req, res) {
   const records = await getAllRecords();
+
   await Promise.all(
     records.map(async (record) => {
-      const refreshToken = record[csvKeys.refreshToken];
-      // requesting access token from refresh token
-      var authOptions = {
-        url: "https://accounts.spotify.com/api/token",
-        headers: {
-          Authorization:
-            "Basic " +
-            new Buffer(client_id + ":" + client_secret).toString("base64"),
-        },
-        form: {
-          grant_type: "refresh_token",
-          refresh_token: refreshToken,
-        },
-        json: true,
-      };
+      const refreshToken = record.fields[csvKeys.refreshToken];
 
-      await new Promise((resolve) => {
-        request.post(authOptions, async function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-            var access_token = body.access_token;
-            await fetchData(access_token, refreshToken);
-            resolve();
-          }
-        });
-      });
+      try {
+        const options = {
+          method: "POST",
+          headers: {
+            Authorization:
+              "Basic " +
+              new Buffer(client_id + ":" + client_secret).toString("base64"),
+            "content-type": "application/x-www-form-urlencoded",
+          },
+          data: qs.stringify({
+            grant_type: "refresh_token",
+            refresh_token: refreshToken,
+          }),
+          url: "https://accounts.spotify.com/api/token",
+        };
+        const response = await axios(options);
+        await fetchData(response.data.access_token, refreshToken);
+      } catch (error) {
+        console.log("~~~ error", error.data);
+      }
     })
   );
 
